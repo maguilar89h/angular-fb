b326707d7b558cce56dd6337ee4e0ead3ef1808c
Manuel Aguilar <maguilar89h@gmail.com>
2018-04-24 22:47:57 -0500

    new commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//fire base
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {environment} from '../environments/environment';
 
+//components
+import { ProductsComponent } from './componets/products/products.component';
+import { ProductsListComponent } from './componets/products/products-list/products-list.component';
+import { Product} from './models/product';
+
+
+//services
+import {ProductService} from './services/product.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductsListComponent,
+    Product
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
